fd660432d398d46cca41126ac9f6805fb47a0e90
MrFayaz786 <mohammedfayaz2003@gmail.com>
2025-12-06 20:37:31 +0530

    task

---
+++ Task-17 Tasks/Task1-5.js
@@ -0,0 +1,20 @@
+// Task 1
+console.log("Mohammed Fayaz M R");
+// Task 2
+alert("Hello! This is an alert message.");
+// Task 3
+  let age = 25; // Replace with your actual age
+  console.log("My age is:", age);
+
+// Task 4
+
+  function greetMorning() {
+    console.log("Good Morning");
+  }
+
+  greetMorning();
+
+// Task 5
+
+
+ // JavaScript is a programming language used to add interactivity to web pages.
